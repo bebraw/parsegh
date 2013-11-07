0d1dd3a3151ea18df4bce42eec1a038d8aefdc96
Juho Vepsalainen <bebraw@gmail.com>
2013-11-07 18:31:56 +0200

    Initial implementation

---
+++ tests/index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+var suite = require('suite.js');
+
+var parseGh = require('../');
+
+
+suite(parseGh, [
+    'foo', {},
+    'git://github.com/coolony/kiwi', {
+        user: 'coolony',
+        repo: 'kiwi'
+    },
+    'git@github.com:jquery/jquery.git', {
+        user: 'jquery',
+        repo: 'jquery'
+    },
+    'https://github.com/mootools/mootools', {
+        user: 'mootools',
+        repo: 'mootools'
+    },
+    'https://github.com/prototype/prototype.git', {
+        user: 'prototype',
+        repo: 'prototype'
+    },
+    'https://github.com/peterwilli/CoolQueue.io', {
+        user: 'peterwilli',
+        repo: 'CoolQueue.io'
+    },
+    'git://github.com/angular-strap/angular-strap.git', {
+        user: 'angular-strap',
+        repo: 'angular-strap'
+    },
+    'https://github.com/nathansmith/960-Grid-System/blob/master/code/css/960.css', {
+        user: 'nathansmith',
+        repo: '960-Grid-System'
+    },
+    'http://fortawesome.github.com/Font-Awesome/', {
+        user: 'fortawesome',
+        repo: 'Font-Awesome'
+    },
+    'http://sapegin.github.io/social-likes/', {
+        user: 'sapegin',
+        repo: 'social-likes'
+    }
+]);
